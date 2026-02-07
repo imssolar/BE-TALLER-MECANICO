b32c7f7c113615721b12c51547b7300d367ebd18
imssolar <i.montecinossolar@gmail.com>
2026-02-07 12:58:13 -0300

    feat(create-marcas-neumaticos.dto): Se agrega validación de la marca al crear una marca

--- src/marcas-neumaticos/dto/create-marcas-neumatico.dto.ts
+++ src/marcas-neumaticos/dto/create-marcas-neumatico.dto.ts
@@ -1,1 +1,9 @@
-export class CreateMarcasNeumaticoDto {}
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
+
+export class CreateMarcasNeumaticoDto {
+
+    @IsString()
+    @IsNotEmpty({ message: 'La marca es requerida' })
+    @MaxLength(15, { message: 'La marca no puede sobrepasar los 15 caracteres' })
+    marca: string;
+}
