71dd081db4aed756552967b46ba1a8ac49fea57e
imssolar <i.montecinossolar@gmail.com>
2026-02-07 15:51:56 -0300

    feat(neumaticos.module.ts): agrega módulos y configuración de TypeORM para la entidad Neumatico

--- src/neumaticos/neumaticos.module.ts
+++ src/neumaticos/neumaticos.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NeumaticosService } from './neumaticos.service';
 import { NeumaticosController } from './neumaticos.controller';
+import { Neumatico } from './entities/neumatico.entity';
+import { BusesModule } from '../buses/buses.module';
+import { EstadosNeumaticoModule } from '../estados-neumatico/estados-neumatico.module';
+import { MarcasNeumaticosModule } from '../marcas-neumaticos/marcas-neumaticos.module';
+import { ModelosNeumaticoModule } from '../modelos-neumatico/modelos-neumatico.module';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Neumatico]),
+    BusesModule,
+    EstadosNeumaticoModule,
+    MarcasNeumaticosModule,
+    ModelosNeumaticoModule,
+  ],
   controllers: [NeumaticosController],
   providers: [NeumaticosService],
+  exports: [NeumaticosService],
 })
 export class NeumaticosModule {}
